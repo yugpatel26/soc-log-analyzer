904e7967bb2aee0d2283dd9c6238d691e29f3d64
Yug Patel <yugp65353@gmail.com>
2026-08-20 17:06:15 +0530

    Added menu system

---
+++ README.md
@@ -0,0 +1,23 @@
+# soc-log-analyzer🔍
+
+A Python-based tool to analyze log files (like SSH/auth logs) and detect suspicious activity — inspired by real SOC analyst workflows.
+
+## 🎯 Features (Planned)
+- [x] Parse log files (CSV/text) for IP, timestamp, and login status
+- [x] Detect brute-force attempts (multiple failed logins from same IP)
+- [x] Flag suspicious IPs based on threshold
+- [ ] Generate readable reports
+- [ ] IOC (Indicator of Compromise) matching against known bad IPs
+- [ ] Interactive dashboard using Streamlit
+
+## 🛠️ Tech Stack
+- Python
+- Streamlit (for dashboard)
+
+## 🚧 Status
+Work in progress — building step by step to learn SOC/SIEM concepts hands-on.
+
+## 📖 How to Run -:
+*(Coming very soon)*
+**
+**
---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+
+
+def load_logs():
+    with open("login_logs.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+         print(f"Time: {row['timestamp']}, IP: {row['ip_address']}, User: {row['username']}, Status: {row['status']}, Location: {row['location']}")
+
+def detect_suspicious_ips():
+    failed_attempts = {}
+    with open("login_logs.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row["status"] == "failed":
+                ip = row["ip_address"]
+                if ip in failed_attempts:
+                    failed_attempts[ip] = failed_attempts[ip] + 1
+                else:
+                    failed_attempts[ip] = 1
+
+    for ip, count in failed_attempts.items():
+        if count >= 3: 
+            print(f"SUSPICIOUS: {ip} had {count} failed login attempts!")
+
+while True:
+    print("----SOC LOG ANALYZER----")
+    print("1. Load Logs")
+    print("2. Detect Suspicious IPs")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        load_logs()
+    elif choice == "2":
+        detect_suspicious_ips()
+    elif choice == "3":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
