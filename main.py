935b7a820a8f15961699522626371b8bc07c9652
Yug Patel <yugp65353@gmail.com>
2026-08-22 16:33:44 +0530

    Added Search feature in menu

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def load_logs():
     with open("login_logs.csv", "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-         print(f"Time: {row['timestamp']}, IP: {row['ip_address']}, User: {row['username']}, Status: {row['status']}, Location: {row['location']}")
+            print(f"Time: {row['timestamp']}, IP: {row['ip_address']}, User: {row['username']}, Status: {row['status']}, Location: {row['location']}")
+
 
 def detect_suspicious_ips():
     failed_attempts = {}
@@ -20,20 +21,37 @@ def detect_suspicious_ips():
                     failed_attempts[ip] = 1
 
     for ip, count in failed_attempts.items():
-        if count >= 3: 
+        if count >= 3:
             print(f"SUSPICIOUS: {ip} had {count} failed login attempts!")
+
+
+def search_logs():
+    search_term = input("Enter IP address or username to search: ")
+    found = False
+    with open("login_logs.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row["ip_address"] == search_term or row["username"] == search_term:
+                print(f"Time: {row['timestamp']}, IP: {row['ip_address']}, User: {row['username']}, Status: {row['status']}, Location: {row['location']}")
+                found = True
+    if not found:
+        print("No records found.")
+
 
 while True:
     print("----SOC LOG ANALYZER----")
     print("1. Load Logs")
     print("2. Detect Suspicious IPs")
-    print("3. Exit")
+    print("3. Search by IP/Username")
+    print("4. Exit")
     choice = input("Enter your choice: ")
     if choice == "1":
         load_logs()
     elif choice == "2":
         detect_suspicious_ips()
     elif choice == "3":
+        search_logs()
+    elif choice == "4":
         print("Exiting...")
         break
     else:
